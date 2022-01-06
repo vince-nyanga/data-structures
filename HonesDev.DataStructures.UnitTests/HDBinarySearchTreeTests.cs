ef2dcaa15ea5af680112add00945f7c16c8b9414
Vincent Nyanga <vince.nyanga@hotmail.com>
2022-01-06 17:58:43 +0200

    bst

---
+++ HonesDev.DataStructures.UnitTests/HDBinarySearchTreeTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HonesDev.DataStructures.BinaryTree;
+using Xunit;
+
+namespace HonesDev.DataStructures.UnitTests
+{
+    public class HDBinarySearchTreeTests
+    {
+        [Fact]
+        public void Insert_WhenEmpty_ShouldSetRoot()
+        {
+            HDBinarySearchTree<int> sut = new();
+
+            sut.Insert(4);
+
+            Assert.Equal(4, sut.Root.Value);
+        }
+
+        [Fact]
+        public void Insert_IfValueGreaterThanRoot_ShouldAddRight()
+        {
+            HDBinarySearchTree<int> sut = new();
+            sut.Insert(4);
+
+            sut.Insert(6);
+
+            Assert.Equal(6, sut.Root.Right.Value);
+        }
+
+        [Fact]
+        public void Insert_IfValueLessThanRoot_ShouldAddLeft()
+        {
+            HDBinarySearchTree<int> sut = new();
+            sut.Insert(4);
+
+            sut.Insert(2);
+
+            Assert.Equal(2, sut.Root.Left.Value);
+        }
+
+    }
+}
---
+++ HonesDev.DataStructures/BinaryTree/HDBinarySearchTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace HonesDev.DataStructures.BinaryTree
+{
+    public class HDBinarySearchTree<T> where T : IComparable
+    {
+        private TreeNode<T> _root;
+        public TreeNode<T> Root { get => _root; }
+
+        public void Insert(T value)
+        {
+            if (_root is null)
+            {
+                _root = new() { Value = value };
+                return;
+            }
+
+            TreeNode<T> node = _root;
+            TreeNode<T> parent = null;
+            TreeNode<T> newNode = new() { Value = value };
+
+            while (node != null)
+            {
+                parent = node;
+                if (value.CompareTo(node.Value) <= 0)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+
+            if (value.CompareTo(parent.Value) <= 0)
+            {
+                parent.Left = newNode;
+            }
+            else
+            {
+                parent.Right = newNode;
+            }
+        }
+    }
+
+    public record TreeNode<T>
+    {
+        public T Value { get; init; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+    }
+}
