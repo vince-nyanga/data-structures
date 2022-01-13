d016e844efde714d328fee59763db2cfa2638ab4
Vincent Nyanga <vince.nyanga@hotmail.com>
2022-01-13 18:13:07 +0200

    binary heap

---
+++ HonesDev.DataStructures.UnitTests/MinHeapTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using HonesDev.DataStructures.BinaryHeap;
+using Xunit;
+
+namespace HonesDev.DataStructures.UnitTests
+{
+    public class MinHeapTests
+    {
+        private readonly Func<int, int, bool> _minHeapComparer = (a, b) => a < b;
+
+        [Fact]
+        public void Insert_IfEmpty_ShouldSetRoot()
+        {
+            HDHeap<int> sut = new(_minHeapComparer);
+
+            sut.Insert(10);
+
+            Assert.Equal(10, sut.Root);
+        }
+
+        [Fact]
+        public void Insert_IfValueLessThanRoot_ShouldSwap()
+        {
+            HDHeap<int> sut = new(_minHeapComparer);
+            sut.Insert(10);
+          
+            sut.Insert(2);
+
+            Assert.Equal(2, sut.Root);
+        }
+
+        [Fact]
+        public void Insert_ShouldCreateMinHeap()
+        {
+            HDHeap<int> sut = new(_minHeapComparer);
+            sut.Insert(10);
+            sut.Insert(2);
+            sut.Insert(3);
+            sut.Insert(1);
+            sut.Insert(7);
+            var expectedResult = "1 2 3 10 7";
+
+            Assert.Equal(expectedResult, sut.ToString());
+        }
+    }
+}
---
+++ HonesDev.DataStructures/BinaryHeap/HDHeap.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HonesDev.DataStructures.BinaryHeap
+{
+    public class HDHeap<T>
+    {
+        private readonly List<T> _items;
+        private readonly Func<T, T, bool> _comparer;
+
+        public HDHeap(Func<T, T, bool> comparer)
+        {
+            _comparer = comparer;
+            _items = new List<T>();
+        }
+
+        public T Root => _items[0];
+
+        public void Insert(T item)
+        {
+            _items.Add(item);
+            var i = _items.Count - 1;
+            while (i > 0)
+            {
+                var parentIndex = GetParentIndex(i);
+                if (_comparer.Invoke(_items[i], _items[parentIndex]))
+                {
+                    Swap(i, parentIndex);
+                    i = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", _items);
+        }
+
+        private int GetParentIndex(int currentIndex) => (currentIndex - 1) >> 1;
+
+        private int GetLeftChildIndex(int currentIndex) => (currentIndex + 1) << 1;
+
+        private int RightChildIndex(int currentIndex) => (currentIndex + 2) << 1;
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            var temp = _items[firstIndex];
+            _items[firstIndex] = _items[secondIndex];
+            _items[secondIndex] = temp;
+        }
+
+    }
+}
