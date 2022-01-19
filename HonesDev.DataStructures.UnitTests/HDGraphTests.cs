cbeeb2eb6967d7bd5a167fcb78dcec13cab0c324
Vincent Nyanga <vince.nyanga@hotmail.com>
2022-01-19 07:31:26 +0200

    graph initial

---
+++ HonesDev.DataStructures.UnitTests/HDGraphTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using HonesDev.DataStructures.Graph;
+using Xunit;
+
+namespace HonesDev.DataStructures.UnitTests
+{
+    public class HDGraphTests
+    {
+        [Fact]
+        public void AddVertex_ShouldAddVertex()
+        {
+            HDGraph<int> sut = new();
+
+            sut.AddVertex(1);
+
+            Assert.Equal(1, sut.Count);
+        }
+
+        [Fact]
+        public void AddVertex_ShouldNotDuplicateVertex()
+        {
+            HDGraph<int> sut = new();
+
+            sut.AddVertex(1);
+            sut.AddVertex(1);
+
+            Assert.Equal(1, sut.Count);
+        }
+
+        [Fact]
+        public void GetTotalConnections_IfNotExist_ShouldThrowException()
+        {
+            HDGraph<int> sut = new();
+
+            Assert.Throws<ArgumentException>(() => sut.GetTotalConnections(1));
+        }
+
+        [Fact]
+        public void GetTotalConnections_ShouldReturnTotalConnections()
+        {
+            HDGraph<int> sut = new();
+            sut.AddVertex(1);
+
+            var result = sut.GetTotalConnections(1);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void AddEdge_ShouldAddUndirectedEdge()
+        {
+            HDGraph<int> sut = new();
+
+            sut.AddEdge(1, 2);
+
+            Assert.Equal(1, sut.GetTotalConnections(1));
+            Assert.Equal(1, sut.GetTotalConnections(2));
+        }
+    }
+}
---
+++ HonesDev.DataStructures/Graph/HDGraph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HonesDev.DataStructures.Graph
+{
+    public class HDGraph<T>
+    {
+        private readonly Dictionary<T, HashSet<T>> _adjacentList;
+
+        public HDGraph()
+        {
+            _adjacentList = new Dictionary<T, HashSet<T>>();
+        }
+
+
+        public void AddVertex(T vertex)
+        {
+            if (!_adjacentList.ContainsKey(vertex))
+            {
+                _adjacentList[vertex] = new HashSet<T>();
+            }
+        }
+
+        public int GetTotalConnections(T vertex)
+        {
+            if (!_adjacentList.ContainsKey(vertex))
+            {
+                throw new ArgumentException($"vertext {vertex} does not exist");
+            }
+
+            return _adjacentList[vertex].Count;
+        }
+
+        public void AddEdge(T source, T destination)
+        {
+            AddVertex(source);
+            AddVertex(destination);
+
+            _adjacentList[source].Add(destination);
+            _adjacentList[destination].Add(source);
+        }
+
+        public int Count => _adjacentList.Keys.Count;
+
+    }
+}
